43f440df3bddc9827d46976614835c9dd5249147
ro-goncalves <ro.go.calves@gmail.com>
2024-04-11 23:17:54 -0300

    Criando interface SelicData

---
+++ src/service/selic/selic.data.interface.ts
@@ -0,0 +1,12 @@
+export interface SelicData {
+    dataCotacao: string,
+    fatorDiario: number
+    media: number
+    mediana: number
+    moda: number
+    desvioPadrao: number
+    indiceCurtose: number
+    financeiro: number
+    qtdOperacoes: number
+    taxaAnual: number
+}
